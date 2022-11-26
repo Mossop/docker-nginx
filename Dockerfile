6a6e8e64df7fa3fbedd0eabf13ba09990af9dce8
Dave Townsend <dtownsend@oxymoronical.com>
2022-11-26 19:58:03 +0000

    Add an additional config directory.

--- Dockerfile
+++ Dockerfile
@@ -4,3 +4,7 @@ RUN apk add --no-cache inotify-tools
 
 COPY 30-watch-config.sh /docker-entrypoint.d
 COPY watch.sh /watch.sh
+COPY nginx.conf /etc/nginx
+COPY default.conf /etc/nginx
+
+RUN mkdir /etc/nginx/generated.d
--- watch.sh
+++ watch.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-while inotifywait -e modify -e delete -e create -e move -r /etc/nginx/conf.d >/dev/null; do
+while inotifywait -e modify -e delete -e create -e move -r /etc/nginx >/dev/null; do
   echo "Reloading nginx..."
   nginx -s reload
 done
